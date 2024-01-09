0216a484d44fd4e1ba2cbd4b7a6a08f2f8f8dd68
eberkin <115997639+eberkin@users.noreply.github.com>
2024-01-09 09:56:03 +0100

    odoo modulua

--- 9. Exekuzioa/st-compose/Addons/txapelketa/__manifest__.py
+++ 9. Exekuzioa/st-compose/Addons/txapelketa/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "txapelketa",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Txapelketaren modulua""",
 
     'description': """
-        Long description of module's purpose
+        enpresaren txapelketaren modulua
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "ST enpresa",
+    'website': "http://stenpresa.eus",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
