206443ae59f0bef28fddc6e9d2b36982660cfb4e
anderibanez123 <anderibanez123@gmail.com>
2024-01-09 12:46:41 +0100

    mvc

---
+++ 9. Exekuzioa/WEB ORRIA/st_WebOrria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace st_WebOrria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
