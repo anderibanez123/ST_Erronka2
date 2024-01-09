e5deaadd4094b3ec21390b7eda27a49914454356
eberkin <115997639+eberkin@users.noreply.github.com>
2024-01-09 12:33:54 +0100

    modeloa

--- 9. Exekuzioa/st-compose/Addons/txapelketa/models/models.py
+++ 9. Exekuzioa/st-compose/Addons/txapelketa/models/models.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class txapelketa(models.Model):
-#     _name = 'txapelketa.txapelketa'
-#     _description = 'txapelketa.txapelketa'
+class txapelketa(models.Model):
+    _name = 'txapelketa.txapelketa'
+    _description = 'txapelketa.txapelketa'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    izena = fields.Char()
+    abizena = fields.Char()
+    nan = fields.Text()
+    puntuaketa = fields.Integer()
+    denbora = fields.Integer()
+ 
