19520b912ad0ab16b05a8915b8efa4c148330a30
eberkin <115997639+eberkin@users.noreply.github.com>
2024-01-10 10:21:41 +0100

    android studio

---
+++ 9. Exekuzioa/app/src/main/java/com/example/st_jokoa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.st_jokoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
