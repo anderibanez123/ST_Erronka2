c75627e561b06a53aa1fa0c5e1f077beeea3770d
eberkin <115997639+eberkin@users.noreply.github.com>
2024-01-10 12:32:27 +0100

    ANDROID STUDIO

--- 9. Exekuzioa/ST_Jokoa/app/src/main/java/com/example/st_jokoa/MainActivity.java
+++ 9. Exekuzioa/ST_Jokoa/app/src/main/java/com/example/st_jokoa/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.st_jokoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void main_btn(View view) {
+        switch (view.getId()){
+            case R.id.btn_play:
+                startActivity(new Intent(MainActivity.this , playActivity.class));
+                break;
+            case R.id.btn_setting:
+                startActivity(new Intent(MainActivity.this , settingActivity.class));
+
+                break;
+            case R.id.btn_exit:
+                this.finishAffinity();
+                break;
+        }
     }
 }
