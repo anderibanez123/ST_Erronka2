fbf02e2d1bf674b60be8de91ddccc7c836ef3cba
anderibanez123 <anderibanez123@gmail.com>
2024-01-11 08:09:08 +0100

    programa

--- 9. Exekuzioa/WEB ORRIA/st_WebOrria/obj/Debug/net8.0/st_WebOrria.AssemblyInfo.cs
+++ 9. Exekuzioa/WEB ORRIA/st_WebOrria/obj/Debug/net8.0/st_WebOrria.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("st_WebOrria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b98b8b2baa930cf180d527c407fa109f718e3a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("st_WebOrria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("st_WebOrria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
